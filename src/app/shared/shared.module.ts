ffadcb0b429998847b48b22aeeff430a80f11816
mellunar <melfer100@gmail.com>
2023-05-25 18:58:51 -0300

    build: install feather icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FieldInputComponent } from './components/field-input/field-input.component';
+import { FeatherModule } from 'angular-feather';
+import {
+  AlertCircle,
+  AlertTriangle,
+  CheckCircle,
+  Check,
+  ChevronDown,
+  Delete,
+  Download,
+  Edit,
+  Plus,
+  X,
+} from 'angular-feather/icons';
+
+const icons = {
+  AlertCircle,
+  AlertTriangle,
+  CheckCircle,
+  Check,
+  ChevronDown,
+  Delete,
+  Download,
+  Edit,
+  Plus,
+  X,
+};
 
 @NgModule({
   declarations: [FieldInputComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [FieldInputComponent],
+  imports: [CommonModule, ReactiveFormsModule, FeatherModule.pick(icons)],
+  exports: [FieldInputComponent, FeatherModule],
 })
 export class SharedModule {}
