ccc9d096fd3d231e1803ee25ee245ecbf36d9f06
mellunar <melfer100@gmail.com>
2023-05-27 19:36:07 -0300

    feat: add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToastModule } from './modules/toast/toast.module';
+import { NavigationComponent } from './modules/navigation/navigation.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ToastModule],
+  imports: [BrowserModule, AppRoutingModule, ToastModule, NavigationComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
