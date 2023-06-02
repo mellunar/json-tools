546bc5b57a4082cb83329159b4e289267e5e9b1c
mellunar <melfer100@gmail.com>
2023-06-02 16:59:11 -0300

    fix: implement textarea component

--- src/app/shared/components/field-textarea/field-textarea.component.ts
+++ src/app/shared/components/field-textarea/field-textarea.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ValueAccessorDirective, injectNgControl } from '../../directives/value-accessor.directive';
 
 @Component({
   selector: 'app-field-textarea',
   templateUrl: './field-textarea.component.html',
-  styleUrls: ['./field-textarea.component.scss']
+  styleUrls: ['./field-textarea.component.scss'],
+  hostDirectives: [ValueAccessorDirective],
 })
 export class FieldTextareaComponent {
+  @Input() compact = true;
+  @Input() label: string;
+  @Input() noPadding = false;
 
+  ngControl = injectNgControl();
 }
